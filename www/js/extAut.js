4eb02bb7ee0671f463c803ab1142f0bd212e0ad6
michelsondan <michelson.dan@gmail.com>
2016-11-15 13:38:33 +0200

    181

---
+++ www/js/extAut.js
@@ -0,0 +1,42 @@
+﻿function aut() {
+    var options = {
+        client_id: '650577198335-qffnsu7cedj38d8sad0c1aikkebdh6t5.apps.googleusercontent.com',
+        client_secret: 'UuEiZoR_1ziSmK0xi85jirjr',
+        redirect_uri: 'http://www.toyou.co.il/dan/aut/token.asp',
+        scopes: ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file']
+    };
+
+    url = window.location.href;
+
+    if (url.indexOf('code') < 0) {
+        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
+            client_id: options.client_id,
+            redirect_uri: options.redirect_uri,
+            response_type: 'code',
+            scope: options.scopes.join(' ')
+        });
+
+        location.href = authUrl;
+    }
+    else {
+        var code = /\?code=(.+)$/.exec(url);
+
+        $.support.cors = true;
+        $.post('https://accounts.google.com/o/oauth2/token', {
+            code: code[1],
+            client_id: options.client_id,
+            client_secret: options.client_secret,
+            redirect_uri: options.redirect_uri,
+            grant_type: 'authorization_code'
+        }).done(function (data) {
+            document.getElementById("txtToken").value = data.access_token;
+        }).fail(function (xhr, status, error) {
+            alert(error);
+        });
+    }
+}
+
+function driveloaded() {
+    aut();
+}
+
---
+++ www/token.asp
@@ -0,0 +1,18 @@
+﻿<%
+    
+    Dim httpRequest,data
+    
+    data ="code=" & request.QueryString("code")
+    data = data & "&client_id=650577198335-qffnsu7cedj38d8sad0c1aikkebdh6t5.apps.googleusercontent.com"
+    data = data & "&client_secret=UuEiZoR_1ziSmK0xi85jirjr"
+    data = data & "&redirect_uri=http://www.toyou.co.il/dan/aut/token.asp"
+    data = data & "&grant_type=authorization_code"
+
+        
+    Set httpRequest = Server.CreateObject("MSXML2.ServerXMLHTTP")
+    httpRequest.Open "POST", "https://accounts.google.com/o/oauth2/token", False
+    httpRequest.SetRequestHeader "Content-Type", "application/x-www-form-urlencoded"
+    httpRequest.Send data
+
+    response.Write(httpRequest.ResponseText)
+%>
